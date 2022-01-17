d5e37de27a3ed89d564657278599770d51139bb9
6691a <6691a@naver.com>
2022-01-17 22:13:21 +0900

    백그라운드 작업

---
+++ 백그라운드/main.py
@@ -0,0 +1,39 @@
+import time
+
+from typing import Optional
+from fastapi import BackgroundTasks, FastAPI, status, Depends
+
+app = FastAPI()
+
+
+def write_log(message: str):
+    time.sleep(2.0)
+
+    with open("log.txt", mode="a") as log:
+        log.write(message)
+
+
+@app.post("/send-notification/{email}", status_code=status.HTTP_202_ACCEPTED)
+async def send_notification(email: str, background_tasks: BackgroundTasks):
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+
+    return {"message": "Message sent"}
+
+
+# 의존성
+def get_query(background_tasks: BackgroundTasks, q: Optional[str] = None):
+    if q:
+        message = f"found query: {q}\n"
+        background_tasks.add_task(write_log, message)
+    return q
+
+
+@app.post("/send-notification/{email}", status_code=status.HTTP_202_ACCEPTED)
+async def send_notification(
+    email: str, background_tasks: BackgroundTasks, q: str = Depends(get_query)
+):
+    message = f"message to {email}\n"
+    background_tasks.add_task(write_log, message)
+
+    return {"message": "Message sent"}
